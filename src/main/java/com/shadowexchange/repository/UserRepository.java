c503d18d0392ec5e268c26c8fb21894cbd79d2ea
shivkumar077 <shivkumar6990655@gmail.com>
2026-08-24 15:50:02 +0530

    creating basic functions

---
+++ src/main/java/com/shadowexchange/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.shadowexchange.repository;
+
+import com.shadowexchange.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
