63fa563242ad23b3d907108c8adcd282de7dd01a
shivkumar077 <shivkumar6990655@gmail.com>
2026-08-25 23:09:16 +0530

    Add fields, constructors, and getters/setters to OrderRequestDTO

--- src/main/java/com/shadowexchange/dto/OrderRequestDTO.java
+++ src/main/java/com/shadowexchange/dto/OrderRequestDTO.java
@@ -1,4 +1,66 @@
 package com.shadowexchange.dto;
 
+import com.shadowexchange.entity.OrderType;
+
+import java.math.BigDecimal;
+
 public class OrderRequestDTO {
+
+    private Long userId;
+    private Long stockId;
+    private OrderType type;
+    private BigDecimal price;
+    private Integer quantity;
+
+    public OrderRequestDTO() {
+    }
+
+    public OrderRequestDTO(Long userId, Long stockId, OrderType type, BigDecimal price, Integer quantity) {
+        this.userId = userId;
+        this.stockId = stockId;
+        this.type = type;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getStockId() {
+        return stockId;
+    }
+
+    public void setStockId(Long stockId) {
+        this.stockId = stockId;
+    }
+
+    public OrderType getType() {
+        return type;
+    }
+
+    public void setType(OrderType type) {
+        this.type = type;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
+
