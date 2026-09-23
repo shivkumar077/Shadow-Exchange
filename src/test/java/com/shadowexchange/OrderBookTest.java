8edad4fb91e95c103240234cdf847e6133fed866
shivkumar077 <shivkumar6990655@gmail.com>
2026-09-24 04:25:22 +0530

    Add test for price-time priority when order prices are equal

--- src/test/java/com/shadowexchange/OrderBookTest.java
+++ src/test/java/com/shadowexchange/OrderBookTest.java
@@ -7,6 +7,8 @@ import com.shadowexchange.entity.Stock;
 import com.shadowexchange.entity.User;
 import com.shadowexchange.orderbook.OrderBook;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrderBookTest {
@@ -51,6 +53,45 @@ public class OrderBookTest {
         assertEquals(
                 new BigDecimal("105.00"),
                 bestBuy.getPrice()
+        );
+    }
+
+    @Test
+    public void olderordergetprioritywhenpriceisequal(){
+
+        User user = new User();
+        Stock stock = new Stock();
+        OrderBook orderBook = new OrderBook();
+
+        Order olderOrder = new Order(
+                user,
+                stock,
+                new BigDecimal("100.00"),
+                10,
+                OrderType.BUY
+        );
+
+        Order newerOrder = new Order(
+                user,
+                stock,
+                new BigDecimal("100.00"),
+                10,
+                OrderType.BUY
+        );
+
+        olderOrder.setCreatedAt(
+                LocalDateTime.of(2026,9,24,10,0));
+        newerOrder.setCreatedAt(
+                LocalDateTime.of(2026,9,24,10,1));
+
+        orderBook.addOrder(olderOrder);
+        orderBook.addOrder(newerOrder);
+
+        Order bestBuy = orderBook.getBestBuy();
+
+        assertEquals(
+                olderOrder,
+                bestBuy
         );
     }
 }
